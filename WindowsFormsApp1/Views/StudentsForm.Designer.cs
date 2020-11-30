24553dfb5d293bb4284ee7b7d2c5042cf755c1fd
filthyman <61003685+filthyman@users.noreply.github.com>
2020-11-30 18:28:48 +0400

    complete homework v2

---
+++ WindowsFormsApp1/Views/StudentsForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace WindowsFormsApp1.Views
+{
+    partial class studentsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.groupGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupGridView
+            // 
+            this.groupGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.groupGridView.Location = new System.Drawing.Point(13, 13);
+            this.groupGridView.Name = "groupGridView";
+            this.groupGridView.Size = new System.Drawing.Size(240, 150);
+            this.groupGridView.TabIndex = 0;
+            // 
+            // studentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupGridView);
+            this.Name = "studentsForm";
+            this.Text = "StudentsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.groupGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView groupGridView;
+    }
+}
